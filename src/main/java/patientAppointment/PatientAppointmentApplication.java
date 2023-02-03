01f813e7b354c3561fad4f6b10757c695f675cca
Angelina <angelina-262695@mail.ru>
2023-02-03 11:13:43 +0300

    Initial commit

---
+++ src/main/java/patientAppointment/PatientAppointmentApplication.java
@@ -0,0 +1,13 @@
+package patientAppointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientAppointmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientAppointmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/patientAppointment/PatientAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package patientAppointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAppointmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
